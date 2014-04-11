508c6fc9f429009c1b4ce3f6168fa0d618dafbbd
g12929mk <g12929mk@s30431.tsuda.ac.jp>
2014-04-11 11:03:37 +0900

    新規にfirst-hello,worldを作成しました

---
+++ src/first/hello/world/FirstHelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package first.hello.world;
+
+/**
+ *
+ * @author g12929mk
+ */
+public class FirstHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("hello,world");
+    }
+}
